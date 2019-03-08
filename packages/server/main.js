a04d37de56ec0a9a33eb48ddc6262d41b8b8420b
akany <sif.mori@gmail.com>
2019-03-08 21:27:55 +0200

    added restore session mock

--- packages/server/main.js
+++ packages/server/main.js
@@ -4,12 +4,25 @@ const app = new Koa()
 const bodyParse = require('koa-bodyparser')
 
 const authRoute = route.post('/api/auth', ctx => {
-  ctx.body = {name: ctx.request.body.email}
+  ctx.body = {
+    name: ctx.request.body.email,
+    token: ctx.request.body.email,
+    playerId: ctx.request.body.email
+  }
+})
+
+const restoreRoute = route.post('/api/auth/restore', async ctx => {
+  ctx.body = {
+    name: ctx.request.body.playerId,
+    token: ctx.request.body.token,
+    playerId: ctx.request.body.playerId
+  }
 })
 
 app.use(bodyParse());
 
 app.use(authRoute)
+app.use(restoreRoute)
 
 app.use(ctx => {
   ctx.body = 'Works'
